d7f37462ed35195950992eb8ca43f40050a9940b
LCL-1729 <alethiametrist@outlook.com>
2024-01-11 15:18:23 -0800

    hullo

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,4 @@
 const origin = "https://deep-index.moralis.io";
 const apiKey= "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJub25jZSI6ImRkYjRhOTI4LWI3ZTEtNDM5My04Yzc0LTk0ZjJjZDVjMmM5YyIsIm9yZ0lkIjoiMzcxODMyIiwidXNlcklkIjoiMzgyMTM1IiwidHlwZUlkIjoiZTg4NmRmMjItODQ1YS00MmU2LTgyZDItNGJmZDE1MWY3ZDEzIiwidHlwZSI6IlBST0pFQ1QiLCJpYXQiOjE3MDUwMTI3NDMsImV4cCI6NDg2MDc3Mjc0M30.7JRYEZNfb9GTE2ytybLZ86MHzNnlmKnD0CirhcNe_kc";
-  
+
+export const getContractNFTs = async () => {};
